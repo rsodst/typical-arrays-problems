48299e83e3ccfea5fadf495ada096b12edf539c5
rsodst <rsodst@gmail.com>
2020-02-19 22:31:53 +0500

    feat: implemented methods

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
+exports.min = function min(array) {
+    return array && array.length > 0 ?
+        array.reduce((min, cur) => {
+            if (cur < min) {
+                min = cur;
+            }
 
-exports.min = function min (array) {
-  return 0;
-}
+            return min;
+        }, array[0]) : 0;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    return array && array.length > 0 ?
+        array.reduce((max, cur) => {
+            if (cur > max) {
+                max = cur;
+            }
 
-exports.avg = function avg (array) {
-  return 0;
-}
+            return max;
+        }, array[0]) : 0;
+};
+
+exports.avg = function avg(array) {
+    return array && array.length > 0 ?
+        array.reduce((sum, cur) => {
+            sum += cur;
+            return sum;
+        }, 0) / array.length : 0;
+};
